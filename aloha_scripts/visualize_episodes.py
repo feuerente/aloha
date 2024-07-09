9d6559ae3fd1caee822bfc8b263e681ec61a2e8a
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2024-07-09 08:48:34 +0200

    Add parts_poses to episode visualization

--- aloha_scripts/visualize_episodes.py
+++ aloha_scripts/visualize_episodes.py
@@ -26,19 +26,21 @@ def load_hdf5(dataset_dir, dataset_name):
         qpos = root['/observations/qpos'][()]
         qvel = root['/observations/qvel'][()]
         effort = root['/observations/effort'][()]
+        parts_poses = root['/observations/parts_poses'][()]
         action = root['/action'][()]
         image_dict = dict()
         for cam_name in root[f'/observations/images/'].keys():
             image_dict[cam_name] = root[f'/observations/images/{cam_name}'][()]
 
-    return qpos, qvel, effort, action, image_dict
+    return qpos, qvel, effort, action, parts_poses, image_dict
+
 
 def main(args):
     dataset_dir = args['dataset_dir']
     episode_idx = args['episode_idx']
     dataset_name = f'episode_{episode_idx}'
 
-    qpos, qvel, effort, action, image_dict = load_hdf5(dataset_dir, dataset_name)
+    qpos, qvel, effort, action, parts_poses, image_dict = load_hdf5(dataset_dir, dataset_name)
     save_videos(image_dict, DT, video_path=os.path.join(dataset_dir, dataset_name + '_video.mp4'))
     visualize_joints(qpos, action, plot_path=os.path.join(dataset_dir, dataset_name + '_qpos.png'))
     visualize_single(effort, 'effort', plot_path=os.path.join(dataset_dir, dataset_name + '_effort.png'))
@@ -147,6 +149,32 @@ def visualize_single(efforts_list, label, plot_path=None, ylim=None, label_overw
     plt.tight_layout()
     plt.savefig(plot_path)
     print(f'Saved effort plot to: {plot_path}')
+    plt.close()
+
+
+def visualize_parts_poses(parts_poses_list, label, plot_path=None, ylim=None, label_overwrite=None):
+    parts_poses = np.array(parts_poses_list)  # (ts, dim)
+    num_ts, num_dim = parts_poses.shape
+    h, w = 2, num_dim
+    num_figs = num_dim
+    fig, axs = plt.subplots(num_figs, 1, figsize=(w, h * num_figs))
+
+    # plot joint state
+    dim_names = ["x", "y", "z", "qw", "qx", "qy", "qz"]
+    for dim_idx in range(num_dim):
+        ax = axs[dim_idx]
+        ax.plot(parts_poses[:, dim_idx], label=label)
+        ax.set_title(f'Dim {dim_idx}: {dim_names[dim_idx]}')
+        ax.legend()
+
+    if ylim:
+        for dim_idx in range(num_dim):
+            ax = axs[dim_idx]
+            ax.set_ylim(ylim)
+
+    plt.tight_layout()
+    plt.savefig(plot_path)
+    print(f'Saved parts_poses plot to: {plot_path}')
     plt.close()
 
 
